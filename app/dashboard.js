fe1123cd6e888bce0ee96efe93dd5c6394178345
Thomas Frentz <thomas@air-de-thomas.home>
2026-04-20 20:31:29 +0200

    Fix realtime client

--- app/dashboard.js
+++ app/dashboard.js
@@ -1,8 +1,8 @@
 'use client'
 import { useState, useEffect } from 'react'
-import { createClient } from '@supabase/supabase-js'
+import { createBrowserClient } from '@supabase/ssr'
 
-const supabase = createClient(
+const supabase = createBrowserClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 )
